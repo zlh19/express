2d507793a64dc8890e2b2a64162b47bc42c23e78
zhanglihao <zhanglihao@qjdchina.com>
2017-02-13 16:13:03 +0800

    部分修改es6

--- server/api/whiteSharesPool.js
+++ server/api/whiteSharesPool.js
@@ -5,15 +5,27 @@ module
 var request = require('request');
 var toys = require( '../module/requestAction' );
 
-function ApiTest(req,res){
-	var options = toys.actionRequest(req,'/as/stockpool/getversion.json');
-    request(options,function(error,response,body){
-        if(!error){
+// function ApiTest(req,res){
+// 	var options = toys.actionRequest(req,'/as/stockpool/getversion.json');
+//     request(options,function(error,response,body){
+//         if(!error){
+//            res.send(body)
+//         }else{
+//            res.send({ code: -1, data: null, msg: 'create action fail'}); 
+//         }
+//     })
+// }
+
+
+let ApiTest=(req,res)=>{
+	let options = toys.actionRequest(req,'/as/stockpool/getversion.json');
+	request(options,(error,response,body)=>{
+		if(!error){
            res.send(body)
         }else{
            res.send({ code: -1, data: null, msg: 'create action fail'}); 
         }
-    })
+	})
 }
 
 module.exports = {
--- server/module/requestAction.js
+++ server/module/requestAction.js
@@ -1,15 +1,34 @@
+'use strict';
 
 var logger	= require( '../logs' );
 var qs = require( 'querystring' );
 var serverConfig = require( '../../config/server' );
 
-function actionRequest(config,path,configSend){
-	var method = config.method.toUpperCase(),
+// function actionRequest(config,path,configSend){
+// 	var method = config.method.toUpperCase(),
+// 		data = ( method == 'GET' ) ? config.query : config.body,
+// 		// path = path + ( method == 'GET' ? '?' + qs.stringify(data) : '' );
+// 		path = path;
+
+// 	var options={
+// 		url:serverConfig.hostname + path,
+// 		method:method,
+// 		json:true,
+// 		body:data
+// 		// form:JSON.stringify(data),
+// 	}
+// 	logger.log('info',options)
+	
+// 	return options
+// }
+
+let actionRequest=(config,paths,configSend)=>{
+	let method = config.method.toUpperCase(),
 		data = ( method == 'GET' ) ? config.query : config.body,
 		// path = path + ( method == 'GET' ? '?' + qs.stringify(data) : '' );
-		path = path;
+		path = paths;
 
-	var options={
+	let options={
 		url:serverConfig.hostname + path,
 		method:method,
 		json:true,
